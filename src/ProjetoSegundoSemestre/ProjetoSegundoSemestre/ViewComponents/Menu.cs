97d033bc2ad7b15b4a6822bc91c0bc509089cb27
lucolborges <lucolborges@hotmail.com>
2022-11-08 17:58:32 -0300

    Tela-Cadastro-Agenda

---
+++ src/ProjetoSegundoSemestre/ProjetoSegundoSemestre/ViewComponents/Menu.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ProjetoSegundoSemestre.ViewComponents
+{
+    public class Menu : Microsoft.AspNetCore.Mvc.ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
